517c19fd9b7092268701e64e6aef02f3c28b77f0
alvinisasi <azraqazravin@gmail.com>
2020-02-13 18:35:53 +0700

    remove debug console

--- src/components/Post.js
+++ src/components/Post.js
@@ -24,7 +24,6 @@ export default function Post(){
 	useEffect(() => {
 		const posts = []
 		ref.get().then(doc => {
-			console.log(doc.data()); 
 			const {title, content, category, date } = doc.data()
 			const postContent = EditorState.createWithContent(convertFromRaw(JSON.parse(content)))
 			posts.push({
@@ -57,66 +56,3 @@ export default function Post(){
 		</Layout>
 	)
 }
-
-/*
-export default class Post extends Component{
-	constructor(props){
-		super(props)
-		this.ref = firebase.firestore().collection('posts').doc('KEpcDy32P3mZerhuQq45')
-		let { id } = useParams()
-    this.state = {
-      posts: [],
-      date: new Date(),
-      editorState: EditorState.createEmpty()
-    }
-	}
-
-  componentDidMount() {
-		
-		const posts = []
-		this.ref.get().then((doc) => {
-			if(doc.exist){
-				const {title, content, category, date } = doc.data()
-				console.log("Document data:", doc.data());
-				const postContent = EditorState.createWithContent(convertFromRaw(JSON.parse(content)))
-				posts.push({
-					key: doc.id,
-					doc,
-					title,
-					content: postContent,
-					category,
-					date
-				})
-				this.setState({
-					posts
-				})		
-			} else {
-				console.log("No such document!");
-			}
-		}).catch(e => {
-			console.log(e)
-		}) 
-  }
-
-	render(){
-		const post = this.state.posts
-		
-		return(
-			<Layout>
-			<h1>{ this.id }</h1>
-				<article className='posts' key={post.key}>
-					<div> 
-						<h1 className='mt0 lh-title'>{post.title}</h1>
-						<span className='f6 dark-gray i'>{post.category}</span>
-						<div className="readonly-editor">
-							<Editor editorState={post.content} plugins={[imagePlugin]} readOnly={true} /> 
-						</div>
-					</div>
-					<p className="f6 lh-copy gray mv0">By <span className="ttu">Alvin Miftah</span></p>
-					<time className="f6 db gray">{post.date}</time>
-				</article>   
-			</Layout>
-		) 
-	}
-}
-*/
